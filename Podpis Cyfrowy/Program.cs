92a5654cc34bdb745de28a8dbcb2180251e7cec2
Kacper Młynarski <kacpermln@wp.pl>
2022-06-18 20:27:43 +0200

    Update Program.cs

--- Podpis Cyfrowy/Program.cs
+++ Podpis Cyfrowy/Program.cs
@@ -14,23 +14,30 @@ public class Program
             {
                 using (SHA256 sha256Hash = SHA256.Create())
                 {
+                    //Przyjecie danych i obliczenie funkcji skrotu
                     Console.WriteLine("Podaj dane do zaszyfrowania:");
                     string source = Console.ReadLine().ToString();
                     byte[] sourceBytes = Encoding.UTF8.GetBytes(source);
                     byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
                     string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
                     Console.WriteLine("Funkcja skrótu tekstu " + source + " to: " + hash);
+
+                    //Zaszyfrowanie danych przy pomocy RSA i odczytanie wartosci kluczu publicznego
                     UnicodeEncoding ByteConverter = new UnicodeEncoding();
                     byte[] dataToEncrypt = ByteConverter.GetBytes(hash);
                     byte[] encryptedData;
                     byte[] decryptedData;
                     encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
-                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
                     string publicKey = Convert.ToBase64String(RSA.ExportRSAPublicKey());
                     Console.WriteLine("Klucz publiczny:");
                     Console.WriteLine(publicKey);
+
+                    //Odszyfrowanie funkcji skrotu
+                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
                     string decryptedString = ByteConverter.GetString(decryptedData);
                     Console.WriteLine("Odszyfrowana funkcja skrótu: {0}", decryptedString);
+
+                    //Porownanie odszyfrowanej funkcji skrotu i pierwotnej
                     if (hash == decryptedString)
                     {
                         Console.WriteLine("Odszyfrowanie udane");
@@ -46,6 +53,8 @@ public class Program
             Console.WriteLine("Blad szyfrowania");
         }
     }
+
+    //Funkcja szyfrujaca
     public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
     {
         try
@@ -65,6 +74,8 @@ public class Program
             return null;
         }
     }
+
+    //Funkcja odszyfrowujaca
     public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
     {
         try
